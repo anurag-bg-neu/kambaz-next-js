29b7f6e858de643310bcfe81c3c76011b7cab7c8
anurag-bg-neu <bheemappagnanamurt.a@northeastern.edu>
2025-10-07 17:47:33 -0400

    toggle dropdown perfected.

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from "react";
 import { ReactNode } from "react";
 import CourseNavigation from "./Navigation";
+import KambazNavigation from "./KambazNavigation";
 import { FaAlignJustify } from "react-icons/fa";
 import { IoIosArrowDown } from 'react-icons/io';
 
@@ -10,7 +11,8 @@ import './styles.css';
 export default function CoursesLayout(
   { children, params }: Readonly<{ children: ReactNode; params: Promise<{ cid: string }> }>) {
 
-  const [showMenu, setShowMenu] = useState(false);
+  const [showMenuCourse, setShowMenuCourse] = useState(false);
+  const [showMenuKambaz, setShowMenuKambaz] = useState(false);
   const [cid, setCid] = useState<string>("");
 
   useEffect(() => {
@@ -20,17 +22,28 @@ export default function CoursesLayout(
   return (
     <div id="wd-courses">
       <h2 className="text-danger d-flex align-items-center">
+        {/* Overlay Kambaz Dropdown for small screens */}
+        {showMenuKambaz && (
+          <div className="overlay-menu-kambaz d-md-none" onClick={() => setShowMenuCourse(false)}>
+            <div className="dropdown-menu-overlay-kambaz" onClick={(e) => e.stopPropagation()}>
+              <KambazNavigation />
+            </div>
+          </div>
+        )}
         <div className="courseBackground">
             <FaAlignJustify
-              id="toggle-icon"
               className="me-4 fs-4 mb-1 d-md-none"
-              onClick={() => setShowMenu(!showMenu)}
+              onClick={() => setShowMenuKambaz(!showMenuKambaz)}
               style={{ cursor: "pointer" }}
             />
             <div className="d-flex align-items-center">
               Courses {cid}
             </div>
-            <div className="d-md-none">
+            <div
+            className="d-md-none"
+            onClick={() => setShowMenuCourse(!showMenuCourse)}
+            style={{ cursor: "pointer" }}
+            >
               <IoIosArrowDown />
             </div>
             <div className="btn btn-secondary btn-lg d-none d-md-block studentView">
@@ -46,8 +59,8 @@ export default function CoursesLayout(
         </div>
 
         {/* Overlay Dropdown for small screens */}
-        {showMenu && (
-          <div className="overlay-menu d-md-none" onClick={() => setShowMenu(false)}>
+        {showMenuCourse && (
+          <div className="overlay-menu d-md-none" onClick={() => setShowMenuCourse(false)}>
             <div className="dropdown-menu-overlay" onClick={(e) => e.stopPropagation()}>
               <CourseNavigation />
             </div>
