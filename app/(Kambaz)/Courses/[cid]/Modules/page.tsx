7ed9085ec98d79b1da499290b9a2dc5a112f2492
anurag-bg-neu <bheemappagnanamurt.a@northeastern.edu>
2025-10-07 17:55:00 -0400

    module and course background screen minwidth added to stop breaking our code.

--- app/(Kambaz)/Courses/[cid]/Modules/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/page.tsx
@@ -8,7 +8,7 @@ export default function Modules() {
   return (
   <div id="module-below-toggle">
     <ModulesControls /><br /><br /><br /><br />
-    <ListGroup className="rounded-0" id="wd-modules" style={{minWidth: "500px"}}>
+    <ListGroup className="rounded-0" id="wd-modules">
       <ListGroupItem className="wd-module p-0 mb-5 fs-5 border-gray">
         <div className="wd-title p-3 ps-2 bg-secondary">
           <BsGripVertical className="me-2 fs-3" /> Week 1 <ModuleControlButtons />
