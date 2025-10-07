f27618b81f76eede5b351ef762d72b61945f7400
anurag-bg-neu <bheemappagnanamurt.a@northeastern.edu>
2025-10-07 04:07:13 -0400

    added hover features.

--- app/Labs/Lab2/Float.tsx
+++ app/Labs/Lab2/Float.tsx
@@ -5,16 +5,46 @@ export default function Float() {
 <div id="wd-float-divs">
  <h2>Float</h2>
  <div>
-   <Image className="wd-float-right" alt="sample-para-1" src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+   <Image
+  className="wd-float-right"
+  width={150}
+  height={30}
+  alt="sample-para-1"
+  src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+/>
    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
-   <Image className="wd-float-left" alt="sample-para-2" src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+   <Image
+  className="wd-float-left"
+  width={150}
+  height={20}
+  alt="sample-para-2"
+  src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+/>
    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
-   <Image className="wd-float-right" alt="sample-para-3" src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
-   <Image className="wd-float-left" alt="sample-para-4" src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+   <Image
+  className="wd-float-right"
+  width={150}
+  height={20}
+  alt="sample-para-3"
+  src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+/>
+   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
+   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
+   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
+   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
+   <Image
+  className="wd-float-left"
+  width={150}
+  height={20}
+  alt="sample-para-4"
+  src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+/>
+   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
+   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
    <div className="wd-float-done"></div>
@@ -28,7 +58,7 @@ export default function Float() {
      Blue </div>
    <div className="wd-float-left wd-dimension-portrait wd-bg-color-red">
      Red </div>
-   <Image className="wd-float-right" alt="starship-image" src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+   <Image className="wd-float-right" width={150} height={20} alt="starship-image" src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
    <div className="wd-float-done"></div>
  </div>
 </div>
