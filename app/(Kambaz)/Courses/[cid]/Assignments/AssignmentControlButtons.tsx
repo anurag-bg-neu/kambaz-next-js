a1acfc73c0e8d4556fb7f0ef6773a45548c4c16d
anurag-bg-neu <bheemappagnanamurt.a@northeastern.edu>
2025-11-26 16:49:27 -0500

    testing1.0

--- app/(Kambaz)/Courses/[cid]/Assignments/AssignmentControlButtons.tsx
+++ app/(Kambaz)/Courses/[cid]/Assignments/AssignmentControlButtons.tsx
@@ -1,12 +1,29 @@
 import { IoAdd, IoEllipsisVertical } from "react-icons/io5";
+import { RootState } from "../../../store";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 
 export default function AssignmentControlButtons() {
+  const { currentUser } = useSelector((state: RootState) => state.accountReducer);
+  const [studentView, setstudentView] =  useState(true);
+
+  useEffect(() => {
+    if ( currentUser && currentUser.role === "STUDENT" ) {
+        setstudentView(true);
+        console.log("Student view enabled");
+    } else {
+        setstudentView(false);
+    }
+  }, [currentUser]);
+
   return (
     <div className="d-flex align-items-center ms-auto me-2" style={{ gap: '8px' }}>
       <div style={{ border: '1px solid black', borderRadius: '20px', padding: '5px 10px' }}>
         40% of Total
       </div>
-      <IoAdd size={24} color="black" />
+      {!studentView &&
+        <IoAdd size={24} color="black" />
+      }
       <IoEllipsisVertical className="fs-4" />
     </div>
   );
